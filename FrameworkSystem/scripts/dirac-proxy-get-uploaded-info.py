a24d041c6c75cd5795ce1b11faba6f021e512d79
Andrii <andrii.lytovchenko.workmail@gmail.com>
2019-10-13 19:30:45 +0000

    Use getDNsForUsername instead getDNForUsername

--- FrameworkSystem/scripts/dirac-proxy-get-uploaded-info.py
+++ FrameworkSystem/scripts/dirac-proxy-get-uploaded-info.py
@@ -48,14 +48,14 @@ if userName in Registry.getAllUsers():
     if userName != proxyProps['username'] and userName != proxyProps['issuer']:
       gLogger.notice("You can only query info about yourself!")
       sys.exit(1)
-  result = Registry.getDNForUsername(userName)
+  result = Registry.getDNsForUsername(userName)
   if not result['OK']:
     gLogger.notice("Oops %s" % result['Message'])
-  dnList = result['Value']
-  if not dnList:
+    sys.exit(1)
+  if not result['Value']:
     gLogger.notice("User %s has no DN defined!" % userName)
     sys.exit(1)
-  userDNs = dnList
+  userDNs = result['Value']
 else:
   userDNs = [userName]
 
