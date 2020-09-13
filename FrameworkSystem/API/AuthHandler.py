80e865bfef927c14a31dbdeddd5ff7b4f5729fd7
TaykYoku <yokutayk@gmail.com>
2020-09-13 16:32:51 +0200

    fix time

--- FrameworkSystem/API/AuthHandler.py
+++ FrameworkSystem/API/AuthHandler.py
@@ -153,7 +153,7 @@ class AuthHandler(WebHandler):
         raise WErr(401, 'Client ID is unregistred.')
       data = {}
       data['expires_in'] = 300
-      data['expires_at'] = int(time()) + data['expires_in']1
+      data['expires_at'] = int(time()) + data['expires_in']
       data['device_code'] = generate_token(20)
       data['user_code'] = generate_token(10)
       data['scope'] = ''
