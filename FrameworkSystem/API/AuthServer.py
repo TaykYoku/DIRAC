6e55943080ea49f23a8c4e8a21b96c949daad9a2
TaykYoku <yokutayk@gmail.com>
2020-09-23 13:49:30 +0200

    in process

--- FrameworkSystem/API/AuthServer.py
+++ FrameworkSystem/API/AuthServer.py
@@ -146,7 +146,7 @@ class DeviceCodeGrant(_DeviceCodeGrant, grants.AuthorizationEndpointMixin):
   def query_user_grant(self, user_code):
     _, data = self.server.getSessionByOption('user_code', user_code)
     print('======= query_user_grant ==========')
-    return (data['userID'], data['group']) if data else None
+    return (data['userID'], data['group']) if data.get('username') else None
 
   def should_slow_down(self, credential, now):
     return False
