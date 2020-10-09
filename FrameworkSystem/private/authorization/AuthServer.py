85f52f93dc5ec0407722fc30eb7feeed7f88a82d
TaykYoku <yokutayk@gmail.com>
2020-10-09 12:39:30 +0200

    in process oidc

--- FrameworkSystem/private/authorization/AuthServer.py
+++ FrameworkSystem/private/authorization/AuthServer.py
@@ -63,14 +63,10 @@ class AuthServer(_AuthorizationServer, SessionManager, ClientManager):
     result = gConfig.getOptionsDictRecursively('/Systems/Framework/Production/Services/AuthManager/AuthorizationServer')
     if result['OK']:
       data = {}
-      print('===> BEFORE')
-      pprint(result['Value'])
-      print('+++ AFTER')
       # Search values with type list
       for key, value in result['Value'].items():
         l = [e for e in value.replace(', ', ',').split(',') if e]
-        data[key] = value if len(l) != 1 else l
-      pprint(data)
+        data[key] = value if len(l) == 1 else l
       # Verify metadata
       metadata = self.metadata_class(data)
       metadata.validate()
