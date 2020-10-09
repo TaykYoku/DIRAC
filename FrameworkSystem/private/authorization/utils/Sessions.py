aa416617a89f1ad21fd3bc3d0ef6df71456b5988
TaykYoku <yokutayk@gmail.com>
2020-10-09 13:16:07 +0200

    in process oidc

--- FrameworkSystem/private/authorization/utils/Sessions.py
+++ FrameworkSystem/private/authorization/utils/Sessions.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from time import time
+
 from DIRAC import gLogger
 from DIRAC.Core.Utilities import ThreadSafe
 from DIRAC.Core.Utilities.DictCache import DictCache
