17d8fb838a1096c8998d2bab12a40536130567dc
TaykYoku <yokutayk@gmail.com>
2020-10-11 23:40:59 +0200

    in process oidc

--- FrameworkSystem/private/authorization/AuthServer.py
+++ FrameworkSystem/private/authorization/AuthServer.py
@@ -157,6 +157,7 @@ class AuthServer(_AuthorizationServer, SessionManager, ClientManager):
 
   def access_token_generator(self, client, grant_type, user, scope):
     print('GENERATE ACCESS TOKEN')
+    print('scope: %s' % scope)
     header = {'alg': 'RS256'}
     payload = {'sub': user,
                'iss': self.metadata['issuer'],
