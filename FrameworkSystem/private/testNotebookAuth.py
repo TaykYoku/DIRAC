ca53ad6700ca996f71b7c0aa7a4b7e29b0118e64
TaykYoku <yokutayk@gmail.com>
2020-10-13 12:09:30 +0200

    in process oidc

--- FrameworkSystem/private/testNotebookAuth.py
+++ FrameworkSystem/private/testNotebookAuth.py
@@ -45,47 +45,47 @@ class notebookAuth(object):
     except Exception as ex:
       return S_ERROR('Cannot read response: %s' % ex)
 
-    def getProxyWithToken(self, token):
-      confUrl = gConfig.getValue("/LocalInstallation/ConfigurationServerAPI")
-      if not confUrl:
-        return S_ERROR('Could not get configuration server API URL.')
-      setup = gConfig.getValue("/LocalInstallation/Setup")
-      if not setup:
-        return S_ERROR('Could not get setup name.')
+  def getProxyWithToken(self, token):
+    confUrl = gConfig.getValue("/LocalInstallation/ConfigurationServerAPI")
+    if not confUrl:
+      return S_ERROR('Could not get configuration server API URL.')
+    setup = gConfig.getValue("/LocalInstallation/Setup")
+    if not setup:
+      return S_ERROR('Could not get setup name.')
 
-      # Get REST endpoints from ConfigurationService
-      try:
-        r = requests.get('%s/option?path=/Systems/Framework/Production/URLs/ProxyAPI' % confUrl, verify=False)
-        r.raise_for_status()
-        proxyAPI = decode(r.text)[0]
-      except requests.exceptions.Timeout:
-        return S_ERROR('Time out')
-      except requests.exceptions.RequestException as e:
-        return S_ERROR(str(e))
-      except Exception as e:
-        return S_ERROR('Cannot read response: %s' % e)
-      
-      # Get proxy
-      url = '%ss:%s/g:%s/proxy?lifetime=%s' % (proxyAPI, setup, group, lifetime)
-      voms = voms or Registry.getGroupOption(group, "AutoAddVOMS", False)
-      if voms:
-        url += '&voms=%s' % voms
-      with OAuth2Session(clientID, token=token) as sess:
-        r = sess.get(url, verify=False)
-        r.raise_for_status()
-      proxy = decode(r.text)[0]
-      if not proxy:
-        return S_ERROR("Result is empty.")
-      
-      proxyLoc = '/tmp/x509up_u%s' % os.getuid()
+    # Get REST endpoints from ConfigurationService
+    try:
+      r = requests.get('%s/option?path=/Systems/Framework/Production/URLs/ProxyAPI' % confUrl, verify=False)
+      r.raise_for_status()
+      proxyAPI = decode(r.text)[0]
+    except requests.exceptions.Timeout:
+      return S_ERROR('Time out')
+    except requests.exceptions.RequestException as e:
+      return S_ERROR(str(e))
+    except Exception as e:
+      return S_ERROR('Cannot read response: %s' % e)
+    
+    # Get proxy
+    url = '%ss:%s/g:%s/proxy?lifetime=%s' % (proxyAPI, setup, group, lifetime)
+    voms = voms or Registry.getGroupOption(group, "AutoAddVOMS", False)
+    if voms:
+      url += '&voms=%s' % voms
+    with OAuth2Session(clientID, token=token) as sess:
+      r = sess.get(url, verify=False)
+      r.raise_for_status()
+    proxy = decode(r.text)[0]
+    if not proxy:
+      return S_ERROR("Result is empty.")
+    
+    proxyLoc = '/tmp/x509up_u%s' % os.getuid()
 
-      self.log.notice('Saving proxy.. to %s..' % proxyLoc)
-      try:
-        with open(proxyLoc, 'w+') as fd:
-          fd.write(proxy.encode("UTF-8"))
-        os.chmod(proxyLoc, stat.S_IRUSR | stat.S_IWUSR)
-      except Exception as e:
-        return S_ERROR("%s :%s" % (proxyLoc, repr(e).replace(',)', ')')))
+    self.log.notice('Saving proxy.. to %s..' % proxyLoc)
+    try:
+      with open(proxyLoc, 'w+') as fd:
+        fd.write(proxy.encode("UTF-8"))
+      os.chmod(proxyLoc, stat.S_IRUSR | stat.S_IWUSR)
+    except Exception as e:
+      return S_ERROR("%s :%s" % (proxyLoc, repr(e).replace(',)', ')')))
 
-      self.log.notice('Proxy is saved to %s.' % proxyLoc)
-      return S_OK()
+    self.log.notice('Proxy is saved to %s.' % proxyLoc)
+    return S_OK()
