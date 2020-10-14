3c36cdeeca82811d3bafba0dcf44e70267d61963
TaykYoku <yokutayk@gmail.com>
2020-10-14 12:09:37 +0200

    in process oidc

--- FrameworkSystem/private/authorization/utils/__init__.py
+++ FrameworkSystem/private/authorization/utils/__init__.py
@@ -3,8 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 from .Clients import Client, ClientRegistrationEndpoint, ClientManager
-from .Sessions import SessionManager
+from .Sessions import SessionManager, Session
 
 __all__ = [
-  'Client', 'ClientRegistrationEndpoint', 'SessionManager', 'ClientManager'
+  'Client', 'ClientRegistrationEndpoint', 'SessionManager', 'ClientManager', 'Session'
 ]
