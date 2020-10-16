c7f6941bd30e8d8c30122bfca3253c40e436b0b8
TaykYoku <yokutayk@gmail.com>
2020-10-16 15:49:46 +0200

    in process oidc

--- FrameworkSystem/Client/AuthManagerClient.py
+++ FrameworkSystem/Client/AuthManagerClient.py
@@ -129,6 +129,6 @@ class AuthManagerClient(Client):
       username, profile, sessionDict = result['Value']
       if username and profile:
         gAuthManagerData.updateProfiles(profile['ID'], profile)
-    return S_OK(username, profile, Session(sessionDict))
+    return S_OK((username, profile, Session(sessionDict)))
 
 gSessionManager = AuthManagerClient()
