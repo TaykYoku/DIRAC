637e79c93f558d352ad32c5bf45d7da4729a6420
TaykYoku <yokutayk@gmail.com>
2020-10-21 23:11:15 +0200

    fix bugs

--- FrameworkSystem/private/authorization/AuthServer.py
+++ FrameworkSystem/private/authorization/AuthServer.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import io
 import json
 from time import time
 from pprint import pprint
