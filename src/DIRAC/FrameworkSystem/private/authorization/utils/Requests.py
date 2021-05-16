6573a980eaebf22e4cb4fdc2c733ef0f4fd12197
TaykYoku <yokutayk@gmail.com>
2021-05-16 12:25:11 +0200

    fix

--- src/DIRAC/FrameworkSystem/private/authorization/utils/Requests.py
+++ src/DIRAC/FrameworkSystem/private/authorization/utils/Requests.py
@@ -20,15 +20,10 @@ class OAuth2Request(_OAuth2Request):
 
         :param list scopes: scopes
     """
-    print('>>>> addScopes')
-    print(self.uri)
     # Remove "scope" argument from uri
     self.uri = re.sub(r"&scope(=[^&]*)?|^scope(=[^&]*)?&?", "", self.uri)
     # Add "scope" argument to uri with new scopes
-    print(self.uri)
-    print(self.scope)
-    print(scopes)
-    self.uri += "&scope=%s" % '+'.join(list(set(scope_to_list(self.scope) + scopes))) or ''
+    self.uri += "&scope=%s" % '+'.join(list(set(scope_to_list(self.scope or '') + scopes))) or ''
     # Reinit all attributes with new uri
     self.__init__(self.method, to_unicode(self.uri))
 
