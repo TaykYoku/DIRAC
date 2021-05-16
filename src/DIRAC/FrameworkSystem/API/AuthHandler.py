bf97129b47f2aaf71bd55df7d923bb70d570e941
TaykYoku <yokutayk@gmail.com>
2021-05-16 22:24:18 +0200

    fix

--- src/DIRAC/FrameworkSystem/API/AuthHandler.py
+++ src/DIRAC/FrameworkSystem/API/AuthHandler.py
@@ -87,9 +87,9 @@ class AuthHandler(TornadoREST):
     cls.server = AuthServer()
     cls.server.css = dict(CSS=cls.CSS, css_align_center=cls.css_align_center, css_main=cls.css_main)
     cls.server.LOCATION = cls.LOCATION
-    cls.idps = IdProviderFactory()
-    cls._idps[cls.server.metadata['issuer']]['jwks_uri'] = cls.server.metadata['jwks_uri']
-    cls._idps[cls.server.metadata['issuer']]['jwks'] = cls.server.db.getJWKs().get('Value', {})
+    # cls.idps = IdProviderFactory()
+    # cls._idps[cls.server.metadata['issuer']]['jwks_uri'] = cls.server.metadata['jwks_uri']
+    # cls._idps[cls.server.metadata['issuer']]['jwks'] = cls.server.db.getJWKs().get('Value', {})
     
   def initializeRequest(self):
     """ Called at every request """
