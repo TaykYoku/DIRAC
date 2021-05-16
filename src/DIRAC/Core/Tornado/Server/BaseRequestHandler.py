2bcf741f072e4613e3b7b6b299e6b0e9f105e447
TaykYoku <yokutayk@gmail.com>
2021-05-16 22:36:51 +0200

    fix

--- src/DIRAC/Core/Tornado/Server/BaseRequestHandler.py
+++ src/DIRAC/Core/Tornado/Server/BaseRequestHandler.py
@@ -343,6 +343,7 @@ class BaseRequestHandler(RequestHandler):
       regardless of the HTTP method used
 
     """
+    self.credDict = self._gatherPeerCredentials()
     try:
       self.credDict = self._gatherPeerCredentials()
     except Exception as e:  # pylint: disable=broad-except
