61b0eb40863ff758c7a1b7661a07f0c7b1857210
TaykYoku <yokutayk@gmail.com>
2021-05-17 16:40:35 +0200

    fix

--- src/DIRAC/FrameworkSystem/DB/AuthDB.py
+++ src/DIRAC/FrameworkSystem/DB/AuthDB.py
@@ -18,6 +18,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from authlib.jose import KeySet, RSAKey, jwk
 from authlib.common.security import generate_token
 from authlib.oauth2.rfc6749.wrappers import OAuth2Token
+from authlib.integrations.sqla_oauth2 import OAuth2TokenMixin
 
 from DIRAC import S_OK, S_ERROR, gLogger, gConfig
 from DIRAC.Core.Base.SQLAlchemyDB import SQLAlchemyDB
