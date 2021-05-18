168a6300d24899c47305c9673abf87f319a9d29f
TaykYoku <yokutayk@gmail.com>
2021-05-18 17:27:21 +0200

    fix

---
+++ src/DIRAC/Resources/IdProvider/DIRACIdProvider.py
@@ -0,0 +1,24 @@
+""" IdProvider based on OAuth2 protocol
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from DIRAC import S_ERROR, S_OK
+from DIRAC.Resources.IdProvider.OAuth2IdProvider import OAuth2IdProvider
+from DIRAC.FrameworkSystem.private.authorization.AuthServer import collectMetadata
+
+__RCSID__ = "$Id$"
+
+
+class DIRACIdProvider(OAuth2IdProvider):
+
+  def fetch_metadata(self, url=None):
+    """ Fetch metada
+    """
+    return collectMetadata(self.metadata['issuer'])
+
+  def researchGroup(self, payload, token):
+    """ Research group
+    """
+    pass
